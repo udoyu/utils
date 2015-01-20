b4d1763ea42caaf4c4d5e7918e49dfa486fa6958
Kenshin <xiyingit@gmail.com>
2015-01-20 17:14:31 +0800

    ...

---
+++ uuid/uuid.go
@@ -0,0 +1,23 @@
+package uuid
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+    "crypto/md5"
+    "strconv"
+    "io"
+)
+
+func NewUuid() string {
+    nano := time.Now().UnixNano()
+    rand.Seed(nano)
+    rndNum := rand.Int63()
+    sessionId := Md5(Md5(strconv.FormatInt(nano, 10))+Md5(strconv.FormatInt(rndNum, 10)))
+}
+
+func Md5(text string) string {
+    hashMd5 := md5.New()
+    io.WriteString(hashMd5, text)
+    return fmt.Sprintf("%x", hashMd5.Sum(nil))
+}
