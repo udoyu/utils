27d64f38214fd93cc0f7104413625fed48b9e783
Kenshin <xiyingit@gmail.com>
2015-01-23 16:59:17 +0800

    ...

---
+++ strings.go
@@ -0,0 +1,30 @@
+package utils
+
+import "strings"
+
+func StringIndexAny(s, cutset string, n int) int {
+    r := 0
+    for n > 0 {
+        n--
+        t := strings.Index(s[r:], cutset)
+        if t == -1 {
+            return -1
+        }
+        r += t + 1
+    }
+    return r-1
+}
+
+func StringCutLeft(s string, offset int) string {
+    if offset < 0 || offset > len(s) {
+        return s
+    }
+    return s[:offset]
+}
+
+func StringCutRight(s string, offset int) string {
+     if offset < 0 || offset > len(s) {
+        return ""
+    }
+    return s[offset:]
+}
