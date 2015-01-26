425cd3c7e6fe46c3f947701af143b6e0f36afacc
Kenshin <xiyingit@gmail.com>
2015-01-26 14:48:13 +0800

    ...

--- struct.go
+++ struct.go
@@ -5,48 +5,12 @@ import (
     "strconv"
 )
 
-func Atol(s string) int64 {
-        i, _ := strconv.ParseInt(s, 10, 64)
-        return i
-}
-
-func Atoi(s string) int {
-        i, _ := strconv.Atoi(s)
-        return i
-}
-
 type QueryInterface interface {
     Query(t reflect.StructField) string
 }
 
-var (
-    STRING_PTR = reflect.TypeOf(proto.String(""))
-    INT32_PTR = reflect.TypeOf(proto.Int32(int32(0)))
-    INT64_PTR = reflect.TypeOf(proto.Int64(int64(0)))
-    UINT32_PTR = reflect.TypeOf(proto.Uint32(uint32(0)))
-    UINT64_PTR = reflect.TypeOf(proto.Uint64(uint64(0)))
-    BOOL_PTR = reflect.TypeOf(proto.Bool(false))
-)
-
-func StructTravelFunc (v reflect.Value, t reflect.StructField, qi QueryInterface) {
-       if str:=qi.Query(t); len(str)!=0 {
-           //form := utils.StringCutRightExp(t.Tag.Get("json"), ",", 1)
-           switch v.Type() {
-               case STRING_PTR : v.Elem().SetString(str)
-               case INT32_PTR : if v.Elem().CanSet() {v.Elem().SetInt(int64(Atoi(str)))}
-               case INT64_PTR : if v.Elem().CanSet() {v.Elem().SetInt(Atol(str))}
-               case UINT32_PTR : if v.Elem().CanSet() {v.Elem().SetUint(uint64(Atoi(str)))}
-               case UINT64_PTR : if v.Elem().CanSet() {v.Elem().SetUint(uint64(Atol(str)))}
-               case BOOL_PTR : if v.Elem().CanSet() {
-                                   if str == "true" {
-                                       v.Elem().SetBool(true)
-                                   } else {
-                                       v.Elem().SetBool(false)
-                                   }
-                               }
-           }
-       }
-
+func StructTravelSet(v interface{}, qi QueryInterface) interface{} {
+    return StructTravel(v, qi, StructTravelFunc)
 }
 
 func StructTravel (v interface{}, 
@@ -61,3 +25,37 @@ func StructTravel (v interface{},
     }
     return v
 }
+
+func StructTravelFunc (v reflect.Value, t reflect.StructField, qi QueryInterface) {
+       if v.Kind() == reflect.Ptr {
+           v = v.Elem()
+           if v.Kind() == reflect.Struct {
+               StructTravel(v, qi, StructTravelFunc)
+           } else if str:=qi.Query(t); len(str)!=0 && v.CanSet() {
+               //form := utils.StringCutRightExp(t.Tag.Get("json"), ",", 1)
+               switch v.Kind() {
+                   case reflect.String: v.SetString(str)
+                   case reflect.Int32 : v.SetInt(int64(Atoi(str)))
+                   case reflect.Int64 : v.SetInt(Atol(str))
+                   case reflect.Uint32 : v.SetUint(uint64(Atoi(str)))
+                   case reflect.Uint64 : v.SetUint(uint64(Atol(str)))
+                   case reflect.Bool : if str == "true" {
+                                           v.Elem().SetBool(true)
+                                       } else {
+                                           v.Elem().SetBool(false)
+                                       }
+               }
+           }
+       }
+}
+
+func Atol(s string) int64 {
+        i, _ := strconv.ParseInt(s, 10, 64)
+        return i
+}
+
+func Atoi(s string) int {
+        i, _ := strconv.Atoi(s)
+        return i
+}
+
