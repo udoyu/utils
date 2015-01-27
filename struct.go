fb74bfd6b86d84a229cd912ab7b3d0dcfaf76fae
Kenshin <xiyingit@gmail.com>
2015-01-27 11:50:08 +0800

    ...

--- struct.go
+++ struct.go
@@ -5,6 +5,30 @@ import (
     "strconv"
 )
 
+func String(s string) *string {
+    return &s
+}
+
+func Int32(i int32) *int32 {
+    return &i
+}
+
+func Int64(i int64) *int64 {
+    return &i
+}
+
+func Uint32(i uint32) *uint32 {
+    return &i
+}
+
+func Uint64(i uint64) *uint64 {
+    return &i
+}
+
+func Bool(b bool) *bool {
+    return &b
+}
+
 type QueryInterface interface {
     Query(t reflect.StructField) string
 }
@@ -28,22 +52,21 @@ func StructTravel (v interface{},
 
 func StructTravelFunc (v reflect.Value, t reflect.StructField, qi QueryInterface) {
        if v.Kind() == reflect.Ptr {
-           v = v.Elem()
-           if v.Kind() == reflect.Struct {
-               //StructTravel(v, qi, StructTravelFunc)
-           } else if str:=qi.Query(t); len(str)!=0 && v.CanSet() {
-               //form := utils.StringCutRightExp(t.Tag.Get("json"), ",", 1)
-               switch v.Kind() {
-                   case reflect.String: v.SetString(str)
-                   case reflect.Int32 : v.SetInt(int64(Atoi(str)))
-                   case reflect.Int64 : v.SetInt(Atol(str))
-                   case reflect.Uint32 : v.SetUint(uint64(Atoi(str)))
-                   case reflect.Uint64 : v.SetUint(uint64(Atol(str)))
-                   case reflect.Bool : if str == "true" {
-                                           v.Elem().SetBool(true)
-                                       } else {
-                                           v.Elem().SetBool(false)
-                                       }
+           if v.NumMethod() == 0 {
+               if str:=qi.Query(t); len(str)!=0 && v.CanSet() {
+                   //form := utils.StringCutRightExp(t.Tag.Get("json"), ",", 1)
+                   switch v.Type() {
+                       case STRING_PTR     : v.Set(reflect.ValueOf(String(str)))
+                       case INT32_PTR      : v.Set(reflect.ValueOf(Int32(int32(Atoi(str)))))
+                       case INT64_PTR      : v.Set(reflect.ValueOf(Int64(Atol(str))))
+                       case UINT32_PTR     : v.Set(reflect.ValueOf(Uint32(uint32(Atoi(str)))))
+                       case UINT64_PTR     : v.Set(reflect.ValueOf(Uint64(uint64(Atol(str)))))
+                       case BOOL_PTR       : if str == "true" {
+                                                 v.Set(reflect.ValueOf(Bool(true)))
+                                             } else {
+                                                 v.Set(reflect.ValueOf(Bool(false)))
+                                             }
+                   }
                }
            }
        }
@@ -58,4 +81,13 @@ func Atoi(s string) int {
         i, _ := strconv.Atoi(s)
         return i
 }
+
+var (
+    STRING_PTR = reflect.TypeOf(String(""))
+    INT32_PTR = reflect.TypeOf(Int32(int32(0)))
+    INT64_PTR = reflect.TypeOf(Int64(int64(0)))
+    UINT32_PTR = reflect.TypeOf(Uint32(uint32(0)))
+    UINT64_PTR = reflect.TypeOf(Uint64(uint64(0)))
+    BOOL_PTR = reflect.TypeOf(Bool(false))
+)
 
