82f2e5868265413f334d47b85091eba1cb2972b4
netyun <554101587@qq.com>
2015-02-09 08:23:45 +0000

    ...

--- beego/mybeego/controller.go
+++ beego/mybeego/controller.go
@@ -1,7 +1,9 @@
 package mybeego
 
 import (
+        "net/http"
 	"github.com/astaxie/beego"
+	"github.com/udoyu/utils/beego/httplib"
 )
 
 type Controller struct {
@@ -17,4 +19,41 @@ func (this *Controller) Query(key string) string {
 
 func (this *Controller) FormValue(key string) string {
     return this.Ctx.Input.Query(key)
+}
+
+func (this *Controller) Transmit(url string) (*httplib.BeegoHttpRequest, error) {
+    r := this.Ctx.Request
+    var b *httplib.BeegoHttpRequest
+    urlString := url
+    if len(r.URL.RawQuery) != 0 {
+        urlString += "?" + r.URL.RawQuery
+    }
+    
+    switch r.Method {
+        case "GET" : b = httplib.Get(urlString)
+        case "POST" : b = httplib.Post(urlString)
+        case "PUT" : b = httplib.Put(urlString)
+        case "DELETE" : b = httplib.Delete(urlString)
+        case "HEAD" : b = httplib.Head(urlString)
+    }
+    if r.Form != nil && len(r.Form) != 0{
+        for k, v := range r.Form {
+            b.Param(k, v[0])
+        }
+    } else {
+            b.Request().Body = r.Body
+    }
+    header := this.GetSession("trans_session")
+    if header != nil {
+	    cookies := header.([]*http.Cookie)
+            req := b.Request()
+	    for _,cookie := range cookies {
+		req.AddCookie(cookie)
+	    }
+    }
+    err := b.Do()
+    if err == nil {
+	this.SetSession("trans_session", b.Response().Cookies())
+    }
+    return b, err
 }
