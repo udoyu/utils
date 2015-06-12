3b08784879f34206522151008ff11c03f6f60994
jettyu <xiyingit@gmail.com>
2015-06-12 17:30:52 +0800

    add set

---
+++ set/safeset.go
@@ -0,0 +1,61 @@
+package test
+
+import (
+	"sync"
+)
+
+type SafeSet struct {
+	set  *Set
+	lock *sync.RWMutex
+}
+
+func NewSafeSet() *SafeSet {
+	s := &SafeSet{
+		set:  NewSet(),
+		lock: new(sync.RWMutex),
+	}
+	return s
+}
+
+func (this *SafeSet) Insert(v interface{}) {
+	this.lock.Lock()
+	this.set.Insert(v)
+	this.lock.Unlock()
+}
+
+func (this *SafeSet) Has(v interface{}) bool {
+	this.lock.RLock()
+	b := this.set.Has(v)
+	this.lock.RUnlock()
+	return b
+}
+
+func (this *SafeSet) Remove(v interface{}) {
+	this.lock.Lock()
+	this.set.Remove(v)
+	this.lock.Unlock()
+}
+
+func (this *SafeSet) Range(callback SetCallback, vs ...interface{}) {
+	this.lock.RLock()
+	defer this.lock.RUnlock()
+	this.set.Range(callback, vs...)
+}
+
+func (this *SafeSet) Size() int {
+	this.lock.RLock()
+	size := this.set.Size()
+	this.lock.RUnlock()
+	return size
+}
+
+func (this *SafeSet) ToSlice() []interface{} {
+	this.lock.RLock()
+	defer this.lock.RUnlock()
+	return this.set.ToSlice()
+}
+
+func (this *SafeSet) Wake() {
+	this.lock.Lock()
+	this.lock.Unlock()
+}
---
+++ set/set.go
@@ -0,0 +1,59 @@
+package set
+
+type SetCallback func(v interface{}, vs ...interface{})
+
+type SetInterface interface {
+	Insert(v interface{})
+	Has(v interface{}) bool
+	Remove(v interface{})
+	Range(callback SetCallback, vs ...interface{})
+	Size() int
+	ToSlice() []interface{}
+	Wake()	//防止读锁一直占用，而写锁被卡住
+}
+
+type Set struct {
+	v map[interface{}]struct{}
+}
+
+func NewSet() *Set {
+	s := &Set{
+		v: make(map[interface{}]struct{}),
+	}
+	return s
+}
+
+func (this *Set) Insert(v interface{}) {
+	this.v[v] = struct{}{}
+}
+
+func (this *Set) Has(v interface{}) bool {
+	_, ok := this.v[v]
+	return ok
+}
+
+func (this *Set) Remove(v interface{}) {
+	delete(this.v, v)
+}
+
+func (this *Set) Range(callback SetCallback, vs ...interface{}) {
+	for k, _ := range this.v {
+		callback(k, vs...)
+	}
+}
+
+func (this *Set) Size() int {
+	return len(this.v)
+}
+
+func (this *Set) ToSlice() []interface{} {
+	arr := make([]interface{}, 0, len(this.v))
+	for k, _ := range this.v {
+		arr = append(arr, k)
+	}
+	return arr
+}
+
+func (this *Set) Wake() {
+	
+}
