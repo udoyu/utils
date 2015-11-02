f1b94bde2128f4604dcba64c4a8531c9b4ad3d22
jettyu <554101587@qq.com>
2015-11-02 13:09:59 +0800

    Add New

--- utils.go
+++ utils.go
@@ -1,1 +1,16 @@
 package utils
+
+import(
+	"reflect"
+)
+
+//s := New(reflect.ValueOf("a")).(string)
+//s = "hello"
+//fmt.Println(s)
+func New(v reflect.Value) interface{} {
+        if v.Kind() == reflect.Ptr {
+                return New(v.Elem())
+        }
+        r := reflect.New(v.Type())
+        return r.Elem().Interface()
+}
