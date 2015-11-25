c13bc62c50118c9b13fe5b72220d4495242e1f78
jettyu <554101587@qq.com>
2015-11-25 17:51:09 +0800

    ...

--- pool/pool.go
+++ pool/pool.go
@@ -1,36 +1,27 @@
 package pool
 
 import (
-	"reflect"
 )
-
-func New(v reflect.Value) interface{} {
-        r := reflect.New(v.Type())
-        if v.Kind() == reflect.Ptr {
-                r.Elem().Set(reflect.New(v.Elem().Type()))
-        }
-        return r.Elem().Interface()
-}
 
 type PoolElem interface{
 	Close()
 }
 
 type Pool struct {
-	elemValue reflect.Value
+	callback func() PoolElem
 	elems chan PoolElem
 	maxSize int32
 }
 
-func NewPool(elem PoolElem, maxSize int32) *Pool {
-	return &Pool{
-		elemValue : reflect.ValueOf(elem),
+func NewPool(callback func() PoolElem, maxSize int32) Pool {
+	return Pool{
+		callback : callback,
 		elems : make(chan PoolElem, maxSize),
 		maxSize : maxSize,
 	}
 }
 
-func (this *Pool) Put(elem PoolElem) {
+func (this Pool) Put(elem PoolElem) {
 	select {
 		case this.elems <- elem :
 			break
@@ -39,11 +30,11 @@ func (this *Pool) Put(elem PoolElem) {
 	}
 }
 
-func (this *Pool) Get() PoolElem {
+func (this Pool) Get() PoolElem {
 	select {
 		case e := <- this.elems :
 			return e
 		default :
-			return New(this.elemValue).(PoolElem)
+			return this.callback()
 	}
 }
