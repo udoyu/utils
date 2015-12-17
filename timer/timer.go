441c265542c4041389ee3502e6f26aadd25dc8fa
jettyu <554101587@qq.com>
2015-12-17 17:59:25 +0800

    ...

--- timer/timer.go
+++ timer/timer.go
@@ -29,21 +29,23 @@ func (this *TimerHandler) add(d time.Duration) *TimeWheel {
 	i := 0
 	if d >= time.Second && d < time.Hour {
 		p = time.Second
-		i = int(d) / int(p)
+		i = int64(d) / int64(p)
 	} else if d < time.Second && d >= time.Millisecond {
 		p = time.Millisecond
-		i = int(d) / int(p)
+		i = int64(d) / int64(p)
 	} else if d < time.Millisecond && d >= time.Microsecond {
 		p = time.Microsecond
-		i = int(d) / int(p)
+		i = int64(d) / int64(p)
 	} else if d < time.Microsecond {
 		p = time.Nanosecond
-		i = int(d) / int(p)
+		i = int64(d) / int64(p)
 	} else if d >= time.Hour {
 		p = time.Hour
-		i = int(d) / int(p)
+		i = int64(d) / int64(p)
 	}
-	if i == 0 {return nil}
+	if i == 0 {
+		return nil
+	}
 	tw := NewTimeWheel(p, int(i))
 	this.Lock()
 	this.timermap[d] = tw
@@ -57,8 +59,8 @@ func (this *TimerHandler) Add(d time.Duration, task func()) {
 		tw = this.add(d)
 	}
 	if tw == nil {
-	    go task()
-	} else { 
-	    tw.Add(task)
+		go task()
+	} else {
+		tw.Add(task)
 	}
 }
